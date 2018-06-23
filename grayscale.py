899663493610e13929d68aaf7503fad805ff3473
Bowen Wang <kevinwang824@outlook.com>
2018-06-23 16:49:38 -0400

    Clean up this part of the code

--- grayscale.py
+++ grayscale.py
@@ -1,14 +1,9 @@
 import cv2
 import numpy as np
-import time
+#import time
 
 # Initialize webcam
 cap = cv2.VideoCapture('project_video.mp4')
-
-# # define range of White color in HSL
-#
-# lower_white = np.array([0,0,0], dtype=np.uint8)
-# upper_white = np.array([0,100,0], dtype=np.uint8)
 
 while cap.isOpened():
 
@@ -18,21 +13,15 @@ while cap.isOpened():
 
     # Convert image from RBG/BGR to GRAYSCALE
     gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Set Threshold to 190-255, everything out of the range will be displayed black
     ret,thresh = cv2.threshold(gray_img,190,255,cv2.THRESH_BINARY)
 
-    # Use inRange to capture only the values between lower & upper_blue
-    # mask = cv2.inRange(hsl_img, lower_white, upper_white)
-
-    # Perform Bitwise AND on mask and our original frame
-    # res = cv2.bitwise_and(frame, frame, mask=mask)
-
+    # Display result
     cv2.imshow('grayimage', gray_img)
     cv2.imshow('thresh', thresh)
-    # cv2.imshow('mask', mask)
-    # cv2.imshow('Filtered Color Only', res)
     if cv2.waitKey(1) == 13:
         break
 
 cap.release()
-
 cv2.destroyAllWindows()
