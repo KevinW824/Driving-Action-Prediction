45e920405479e079609c7450af9e5a8880836b43
Bowen Wang <kevinwang824@outlook.com>
2018-06-25 17:19:11 -0400

    PerspectiveTransform
    Best I can do as perspective transform for now, definitely need this later, but gonna try new method of finding lanes now.

--- OjbectTracking.py
+++ OjbectTracking.py
@@ -5,18 +5,103 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 im = cv2.imread('./data/frame5.jpg')
-img = im[:665,:]
+ori = im[:665,:]
 
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_img = cv2.cvtColor(ori, cv2.COLOR_BGR2GRAY)
 
-ret,thresh = cv2.threshold(gray_img,195,255,cv2.THRESH_BINARY)
-cv2.imshow('frame5', thresh)
+ret,img = cv2.threshold(gray_img,195,255,cv2.THRESH_BINARY)
+cv2.imshow('frame5', img)
 
-col = np.sum(thresh, axis=0)
-convert = col/255
+def add_lines(img, src):
+    img2 = np.copy(img)
+    color = [255, 0, 0] # Red
+    thickness = 2
+    x0, y0 = src[0]
+    x1, y1 = src[1]
+    x2, y2 = src[2]
+    x3, y3 = src[3]
+    cv2.line(img2, (x0, y0), (x1, y1), color, thickness)
+    cv2.line(img2, (x1, y1), (x2, y2), color, thickness)
+    cv2.line(img2, (x2, y2), (x3, y3), color, thickness)
+    cv2.line(img2, (x3, y3), (x0, y0), color, thickness)
+    return img2
 
-plt.plot(convert)
+
+def add_point (img, src):
+    img2 = np.copy(img)
+    color = [255, 0, 0] # Red
+    thickness = -1
+    radius = 15
+    x0, y0 = src[0]
+    x1, y1 = src[1]
+    x2, y2 = src[2]
+    x3, y3 = src[3]
+    cv2.circle(img2, (x0, y0), radius, color, thickness)
+    cv2.circle(img2, (x1, y1), radius, color, thickness)
+    cv2.circle(img2, (x2, y2), radius, color, thickness)
+    cv2.circle(img2, (x3, y3), radius, color, thickness)
+    return img2
+
+src = np.float32([
+    [225, 660],
+    [590, 450],
+    [695, 450],
+    [1075, 660]
+])
+# Points for the new image
+dst = np.float32([
+    [400, 720],
+    [400, 0],
+    [880, 0],
+    [880, 720]
+])
+
+
+def warper(img):
+    # Compute and apply perpective transform
+    img_size = (img.shape[1], img.shape[0])
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
+
+    return warped
+
+
+def unwarp(img):
+    # Compute and apply inverse perpective transform
+    img_size = (img.shape[1], img.shape[0])
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    unwarped = cv2.warpPerspective(img, Minv, img_size, flags=cv2.INTER_NEAREST)
+
+    return unwarped
+
+
+warped = warper(img)
+
+# add the points to the og and warped images
+src_points_img = add_point(img, src)
+src_points_img = add_lines(src_points_img, src)
+dst_points_warped = add_point(warped, dst)
+dst_points_warped = add_lines(dst_points_warped, dst)
+
+### Plot the source points on the original image and the warped image
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(src_points_img)
+ax1.set_title('Original Image with Source Points', fontsize=25)
+ax2.imshow(dst_points_warped)
+ax2.set_title('Warped Perspective', fontsize=25)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
+
+# points_A = np.float32([[575, 445], [655, 445], [340, 665], [1110, 665]])
+#
+# points_B = np.float32([[0, 0], [770, 0], [0, 220], [770, 220]])
+#
+# M = cv2.getPerspectiveTransform(points_A, points_B)
+#
+# warped = cv2.warpPerspective(thresh, M, (770, 220))
+#
+# cv2.imshow('warpPerspective', warped)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- grayscale.py
+++ grayscale.py
@@ -8,67 +8,6 @@ import os
 import os.path
 from pathlib import Path
 
-
-# def calc_line_fits_from_prevcalc_line(img, leftLine, rightLine):
-#
-#     left_fit = leftLine.best_fit_px
-#     right_fit = rightLine.best_fit_px
-#
-#     ### Settings
-#     margin = 100  # Width on either side of the fitted line to search
-#
-#     nonzero = img.nonzero()
-#     nonzeroy = np.array(nonzero[0])
-#     nonzerox = np.array(nonzero[1])
-#
-#     left_lane_inds = ((nonzerox > (left_fit[0] * (nonzeroy ** 2) + left_fit[1] * nonzeroy + left_fit[2] - margin)) & (
-#     nonzerox < (left_fit[0] * (nonzeroy ** 2) + left_fit[1] * nonzeroy + left_fit[2] + margin)))
-#     right_lane_inds = ((nonzerox > (right_fit[0] * (nonzeroy ** 2) + right_fit[1] * nonzeroy + right_fit[2] - margin)) & (
-#     nonzerox < (right_fit[0] * (nonzeroy ** 2) + right_fit[1] * nonzeroy + right_fit[2] + margin)))
-#
-#     # Extract left and right line pixel positions
-#     leftx = nonzerox[left_lane_inds]
-#     lefty = nonzeroy[left_lane_inds]
-#     rightx = nonzerox[right_lane_inds]
-#     righty = nonzeroy[right_lane_inds]
-#
-#     # Fit a second order polynomial to each
-#     left_fit = np.polyfit(lefty, leftx, 2)
-#     right_fit = np.polyfit(righty, rightx, 2)
-#
-#     # Fit a second order polynomial to each in meters
-#     left_fit_m = np.polyfit(lefty * ym_per_pix, leftx * xm_per_pix, 2)
-#     right_fit_m = np.polyfit(righty * ym_per_pix, rightx * xm_per_pix, 2)
-#
-#     # Generate x and y values for plotting
-#     ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
-#     left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-#     right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
-#
-#     # Create an image to draw on and an image to show the selection window
-#     out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
-#     window_img = np.zeros_like(out_img)
-#
-#     # Color in left and right line pixels
-#     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-#     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-#
-#     # Generate a polygon to illustrate the search window area
-#     # And recast the x and y points into usable format for cv2.fillPoly()
-#     left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
-#     left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
-#     left_line_pts = np.hstack((left_line_window1, left_line_window2))
-#     right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
-#     right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
-#     right_line_pts = np.hstack((right_line_window1, right_line_window2))
-#
-#     # Draw the lane onto the warped blank image
-#     cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-#     cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-#     result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-#
-#     return left_fit, right_fit, left_fit_m, right_fit_m, result
-
 # Create folder to save frames
 try:
     if not os.path.exists('data'):
