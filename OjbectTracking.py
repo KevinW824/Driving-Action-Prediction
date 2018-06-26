01840d98a79abfbe8a5707117be751d8fded9e78
Bowen Wang <kevinwang824@outlook.com>
2018-06-26 13:18:35 -0400

    I

--- OjbectTracking.py
+++ OjbectTracking.py
@@ -1,22 +1,53 @@
+import glob
 import cv2
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import time
+import os
+import os.path
+from pathlib import Path
+np.set_printoptions(threshold=np.inf)
 
-im = cv2.imread('./data/frame5.jpg')
-img = im[:665,:]
+im_thresh = cv2.imread('./data/frame0_thresh.jpg')
+im_origin = cv2.imread('./data/frame0_frame.jpg')
+im_gray = cv2.imread('./data/frame0_gray.jpg')
+img_thresh = im_thresh[430:665,:]
+img_origin = im_origin[430:665,:]
+img_gray = im_gray[430:665,:]
 
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def gray_scale(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
 
-ret,thresh = cv2.threshold(gray_img,195,255,cv2.THRESH_BINARY)
-cv2.imshow('frame5', thresh)
 
-col = np.sum(thresh, axis=0)
-convert = col/255
+def binary_threshold(img):
+    ret,img = cv2.threshold(img,175,255,cv2.THRESH_BINARY)
+    return img
 
-plt.plot(convert)
-plt.show()
+
+def gaussian_blur(img):
+    img = cv2.GaussianBlur(img, (7,7), 0)
+    return img
+
+gimg = gray_scale(img_thresh)
+timg = binary_threshold(gimg)
+
+lst_row = timg[234:235,:]
+# lst_row.tolist().index(255)
+
+
+# col = np.sum(thresh, axis=0)
+# convert = col/255
+#
+# plt.plot(convert)
+# plt.show()
+
+# ret,thresh = cv2.threshold(Gimg,175,255,cv2.THRESH_BINARY)
+cv2.imshow('frame0_thresh', timg)
+cv2.imshow('lst_raw',lst_row)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- grayscale.py
+++ grayscale.py
@@ -94,7 +94,7 @@ while cap.isOpened():
     # Binary Thresholding
     # Set Threshold to 190-255
     # Everything out of the range will be displayed black
-    ret,thresh = cv2.threshold(gray_img,195,255,cv2.THRESH_BINARY)
+    ret,thresh = cv2.threshold(gray_img,175,255,cv2.THRESH_BINARY)
 
 
     # Display result
@@ -103,11 +103,11 @@ while cap.isOpened():
     cv2.imshow('thresh', thresh)
 
     # Save each frames
-    # name = './data/frame' + str(currentFrame) + '.jpg'
-    # print('Creating...' + name)
-    # cv2.imwrite(name, thresh)
+    name = './data/frame' + str(currentFrame) + '.jpg'
+    print('Creating...' + name)
+    cv2.imwrite(name, gray_img)
 
-    # currentFrame += 1
+    currentFrame += 1
 
 
 
